f8bfbf8517102034173e52ea479b6d1a9d78afc0
gauravl-tevaeralabs <gauravl@tevaeralabs.com>
2025-03-22 13:24:46 +0530

    [Vesting Wallet] Overriden releasable(token) signature

--- contracts/VestingWalletCliffUpgradeable.sol
+++ contracts/VestingWalletCliffUpgradeable.sol
@@ -116,11 +116,35 @@ contract VestingWalletCliffUpgradeable is VestingWalletUpgradeable {
     }
 
     /**
+     * @dev Overriden getter for the amount of releasable `token` tokens. `token` should be the address of an
+     * {IERC20} contract.
+     */
+    function releasable(
+        address token
+    ) public view virtual override returns (uint256) {
+        return vestedAmount(token, uint64(block.timestamp)) - released(token);
+    }
+
+    /**
      * @dev Overriden method which calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
      */
     function vestedAmount(
         uint64 timestamp
     ) public view virtual override returns (uint256) {
         return _vestingSchedule(amount, timestamp);
+    }
+
+    /**
+     * @dev Overriden method which calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
+     */
+    function vestedAmount(
+        address token,
+        uint64 timestamp
+    ) public view virtual override returns (uint256) {
+        return
+            _vestingSchedule(
+                IERC20(token).balanceOf(address(this)) + released(token),
+                timestamp
+            );
     }
 }
